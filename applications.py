93c5bbe2fe5fc5d7f7e7577b5a7b5a4bbb5f3b8b
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-12-10 00:22:00 +0500

    duplicte val dict .

--- applications.py
+++ applications.py
@@ -10,9 +10,9 @@
 # print(words_count)
 
 # Write a Python program to find the key with the maximum value in the diction
-val = {'a': 10, 'b': 15, 'c': 7}
-max_val = max(val, key=val.get)
-print(max_val)
+# val = {'a': 10, 'b': 15, 'c': 7}
+# max_val = max(val, key=val.get)
+# print(max_val)
 
 
 # Maping square
@@ -21,5 +21,20 @@ print(max_val)
 # }
 # print(sq)
 
+
+
+# Write a Python program to remove duplicate values from the dictionary 
+duplicate = {'a': 10, 'b': 15, 'c': 10, 'd': 15}
+new_dict = {}
+new_val =set()
+
+for key, value in duplicate.items():
+    if value not in new_val:
+        new_dict[key] = value
+        new_val.add(value)
+
+print('old doubled values dic: ',duplicate)
+print('new single value dic: ',new_dict)
+    
 
 
