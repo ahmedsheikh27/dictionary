3f6fe825abac1e470aab7c1333c41e23dbc66cd5
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-12-10 00:11:23 +0500

    updated

--- merge.py
+++ merge.py
@@ -1,10 +1,15 @@
 dict1 = {'a': 1, 'b': 2}  
-dict2 = {'a': 1, 'b': 9}   
+# dict2 = {'a': 1, 'b': 3}  #printing false on change value to 3 
+dict2 = {'a': 1, 'b': 2}   
+
+# adding dict2 to dict1 
 
 # dict1.update(dict2)
 # print(f'{dict1}')
 
 # next question
+
+# checking 2 dict are equal 
 onekey = dict1.items()
 seckey = dict2.items()
 if onekey == seckey:
