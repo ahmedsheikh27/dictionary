20a5a1c5510ec2a5786dcb2bbb859edf18dd5647
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-12-10 00:11:15 +0500

    word count, map ing square done

--- applications.py
+++ applications.py
@@ -1,16 +1,25 @@
-text = "hello world hello python world"
+# text = "hello world hello python world"
 
-words = text.split()
+# words = text.split()
 
-words_count = {}
-for word in words:
-    words_count[word] = words_count.get(word, 0) +1
+# words_count = {}
+# for word in words:
+#     words_count[word] = words_count.get(word, 0) +1
 
 
-print(words_count)
+# print(words_count)
 
 # Write a Python program to find the key with the maximum value in the diction
-# val = {'a': 10, 'b': 15, 'c': 7}
+val = {'a': 10, 'b': 15, 'c': 7}
+max_val = max(val, key=val.get)
+print(max_val)
+
+
+# Maping square
+# sq = { x:x**2 
+#       for x in range(1,6)
+# }
+# print(sq)
 
 
 
